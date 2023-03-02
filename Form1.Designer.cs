177d4caa365bcad49a7f25ab7e454f3391bcdf20
ultron01 <p.vurm@outlook.cz>
2023-03-02 21:25:44 +0100

    Přidejte soubory projektu.

---
+++ Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace WordFinder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblTitulek = new System.Windows.Forms.Label();
+            this.lblSlovo = new System.Windows.Forms.Label();
+            this.lblText = new System.Windows.Forms.Label();
+            this.tboxSlovo = new System.Windows.Forms.TextBox();
+            this.tboxText = new System.Windows.Forms.TextBox();
+            this.btnHledat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitulek
+            // 
+            resources.ApplyResources(this.lblTitulek, "lblTitulek");
+            this.lblTitulek.BackColor = System.Drawing.Color.DarkOrange;
+            this.lblTitulek.Name = "lblTitulek";
+            // 
+            // lblSlovo
+            // 
+            resources.ApplyResources(this.lblSlovo, "lblSlovo");
+            this.lblSlovo.Name = "lblSlovo";
+            // 
+            // lblText
+            // 
+            resources.ApplyResources(this.lblText, "lblText");
+            this.lblText.Name = "lblText";
+            // 
+            // tboxSlovo
+            // 
+            resources.ApplyResources(this.tboxSlovo, "tboxSlovo");
+            this.tboxSlovo.Name = "tboxSlovo";
+            // 
+            // tboxText
+            // 
+            resources.ApplyResources(this.tboxText, "tboxText");
+            this.tboxText.Name = "tboxText";
+            // 
+            // btnHledat
+            // 
+            resources.ApplyResources(this.btnHledat, "btnHledat");
+            this.btnHledat.Name = "btnHledat";
+            this.btnHledat.UseVisualStyleBackColor = true;
+            this.btnHledat.Click += new System.EventHandler(this.btnHledat_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnHledat);
+            this.Controls.Add(this.tboxText);
+            this.Controls.Add(this.tboxSlovo);
+            this.Controls.Add(this.lblText);
+            this.Controls.Add(this.lblSlovo);
+            this.Controls.Add(this.lblTitulek);
+            this.HelpButton = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.HelpButtonClicked += new System.ComponentModel.CancelEventHandler(this.Form1_HelpButtonClicked);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulek;
+        private System.Windows.Forms.Label lblSlovo;
+        private System.Windows.Forms.Label lblText;
+        private System.Windows.Forms.TextBox tboxSlovo;
+        private System.Windows.Forms.TextBox tboxText;
+        private System.Windows.Forms.Button btnHledat;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WordFinder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHledat_Click(object sender, EventArgs e)
+        {
+            if(tboxSlovo.Text == "")
+            {
+                SystemSounds.Asterisk.Play();
+                MessageBox.Show("Nezadal jsi hledané slovo!", "Chyba!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else if(tboxText.Text == "")
+            {
+                SystemSounds.Asterisk.Play();
+                MessageBox.Show("Nezadal jsi prohledávaný text!", "Chyba!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else
+            {
+                int pocet = 0;
+                int slov = 0;
+                int radek = 0;
+                string pozice = "";
+                string tboxtext = tboxText.Text;
+                tboxtext = tboxtext.Replace(".", "");
+                tboxtext = tboxtext.Replace(",", "");
+                tboxtext = tboxtext.Replace("!", "");
+                tboxtext = tboxtext.Replace("?", "");
+                tboxtext = tboxtext.Replace("(", "");
+                tboxtext = tboxtext.Replace(")", "");
+                tboxtext = tboxtext.Replace("\"", "");
+                tboxtext = tboxtext.Replace("\r", "");
+                string[] poleText = tboxtext.Split('\n');
+
+                pozice += "(pozice slova), (číslo řádku) ";
+
+                foreach (string slovo in poleText)
+                {
+                    /* slov++;
+                    foreach(char znak in slovo)
+                    {
+                        if(znak == '\n')
+                        {
+                            radek++;
+                        }
+                    }
+                    if (slovo.ToLower() == tboxSlovo.Text.ToLower())
+                    {
+                        pozice += $"{slov}, {radek}; ";
+                        pocet++;
+                    } */
+                    radek++;
+                    foreach(string neco in slovo.Split(' '))
+                    {
+                        slov++;
+                        if(neco.ToLower() == tboxSlovo.Text.ToLower())
+                        {
+                            pozice += $"{slov}, {radek}; ";
+                            pocet++;
+                        }
+                    }
+                }
+
+                pozice = pozice.Remove(pozice.Length - 2);
+
+                if (pocet > 0)
+                {
+                    SystemSounds.Question.Play();
+                    MessageBox.Show($"Hledané slovo '{tboxSlovo.Text}' se v textu vyskytují {pocet} krát.\n\nDélka hledaného slova: {tboxSlovo.Text.Length} znaků\nSlov celkem: {slov}\nSlovo nalezeno na pozicích: {pozice}", "Informace", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                } else
+                {
+                    SystemSounds.Question.Play();
+                    MessageBox.Show($"Hledané slovo '{tboxSlovo.Text}' se v textu nikde nevyskytuje.", "Informace", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void Form1_HelpButtonClicked(object sender, CancelEventArgs e)
+        {
+            SystemSounds.Question.Play();
+            MessageBox.Show($"Aplikace WordFinder\n2023 © Petr Vurm\n\nVytvořeno jako domácí úkol z předmětu PROGRAMOVÁNÍ na SPŠ Hradební.", "O aplikaci", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
